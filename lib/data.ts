09934b47cd0765312b757778c68f9ca9b3b42a4d
HHuzaifa <huzaifa50100@gmail.com>
2023-08-23 04:55:56 +0500

    .

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import geNius from "@/public/genius.png";
+import reDesigner from "@/public/redesigner.png";
+import moVix from "@/public/movix.png";
 
 export const links = [
   {
@@ -35,73 +35,79 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Machine Learning Engineer",
+    location: "Remote",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Providing cutting-edge Machine Learning solutions to the freelance tech landscape.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2020 - Present",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Machine Learning Engineer | React/Next.Js Developer",
+    location: "Remote",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "Exploring The Fiverr freelance platform and providing cutting-edge Machine Learning and web app solutions.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023 - Present",
+  },
+  {
+    title: "Machine Learning Engineer | React/Next.Js Developer",
+    location: "Remote",
+    description:
+      "Exploring The Upwork freelance platform and providing cutting-edge Machine Learning and web app solutions.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023 - Present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "ReDesigner",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    tags: ["Replicate AI", "Next.js", "PlanetScale", "Tailwind", "Prisma", "Typescript"],
+    imageUrl: reDesigner,
   },
   {
-    title: "rmtDev",
+    title: "Genius",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Replicate AI", "Open AI", "Stripe", "Prisma"],
+    imageUrl: geNius,
   },
   {
-    title: "Word Analytics",
+    title: "Movix",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    tags: ["React", "IMDB API", "Tailwind",, "Javascript", "Axios", "Redux"],
+    imageUrl: moVix,
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
+  "Python",
+  "TensorFlow",
+  "HugginFace",
+  "Langchain",
+  "Pandas",
+  "NumPy",
+  "Matplotlib",
+  "PyTorch",
+  "OpenCV",
+  "MLFlow",
+  "KubeFlow",
+  "Kubernetes",
+  "Docker",
+  "AWS",
+  "PyCaret",
   "JavaScript",
   "TypeScript",
   "React",
   "Next.js",
-  "Node.js",
   "Git",
   "Tailwind",
+  "Figma",
   "Prisma",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
