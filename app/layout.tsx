5bd9e479913ddcbf96decc8413b0932b59c9115c
HHuzaifa <huzaifa50100@gmail.com>
2023-08-22 05:09:42 +0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "MAVERICK",
+  description:
+    "Huzaifa/Maverick is a machine learning engineer and a React/Next.js developer with 3 years of experience.",
 };
 
 export default function RootLayout({
